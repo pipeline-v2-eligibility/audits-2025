c660c004a9833c5e0c5beaef70d784592a79cdbc
Charles Opute Odili <chaluwa@gmail.com>
2022-08-01 07:23:07 +0000

    build backend tests

---
+++ backend/limiter.js
@@ -0,0 +1,83 @@
+const axios = require('axios');
+const properties = process.CI === true ? require('../../properties.json') : {};
+
+let cases = [];
+let maybe = test.skip;
+
+const delay = ({until: timeout}) => new Promise((resolve) => {
+    setTimeout(() => {
+        resolve();
+    }, timeout);
+});
+
+if (properties && properties.apiBaseURL && properties.apiBaseURL !== '') {
+    maybe = test;
+    axios.defaults.baseURL = properties.apiBaseURL;
+}
+
+const getRandomTestData = async () => {
+    if (maybe === test.skip) return;
+
+    const URL = 'https://randomapi.com/api/j5vghihb?key=LEIX-GF3O-AG7I-6J84';
+    const { data: { results } } = await axios.get(URL);
+    const [data] = results;
+    cases = data;
+};
+
+beforeAll(() => {
+    return getRandomTestData();
+});
+
+maybe('Backend API - Rate limiter allows calls within limit', async () => {
+    const calls = cases.happy?.map(schedule => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                await delay({until: schedule});
+                const { data } = await axios('/howold?dob=436504400000');
+                resolve(data);
+            } catch (error) {
+               reject(error); 
+            }
+        });
+    });
+
+    try {
+        const results = await Promise.all(calls);
+        for (let data of results) {
+            expect(data).toHaveProperty('age');
+            expect(data.age).toBeGreaterThanOrEqual(0);
+        }
+    } catch (error) {
+        console.warn(error);
+        fail('Calling the API within the calls limit should not fail with any error');
+    }
+});
+
+maybe('Backend API - Rate limiter flags too many calls', async () => {
+    console.log(cases.edges);
+    const calls = cases.edges?.map(schedule => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                await delay({until: schedule});
+                const { data } = await axios('/howold?dob=436504400000');
+                resolve(data);
+            } catch (error) {
+               reject(error); 
+            }
+        });
+    });
+
+    try {
+        await Promise.all(calls);
+    } catch (error) {
+        if (error.response?.status) {
+            console.log('status', error.response?.status);
+            expect(error.response?.status).toBe(429);
+        }
+        expect(error.response?.data?.error).toBeDefined();
+        expect(typeof error.response?.data?.error).toBe('string'); 
+
+        expect(error.response?.headers['x-ratelimit-limit']).toBeDefined();
+        expect(error.response?.headers['x-ratelimit-remaining']).toBeDefined();
+    }
+});
