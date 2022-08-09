bf10295cdde8b3ba7a5072fb917cb89a9e467dbe
Charles Opute Odili <chaluwa@gmail.com>
2022-08-09 11:36:14 +0000

    test scans

--- cloud/__tests__/vulnerabilities.js
+++ cloud/__tests__/vulnerabilities.js
@@ -1,19 +1,28 @@
 const axios = require('axios');
+const fileExists = require('fs.promises.exists');
 const properties = require('../../../properties.json');
 
+let repo;
+let owner;
+let octokit;
 let cases = {};
 let maybe = test.skip;
-jest.setTimeout(20000);
+jest.setTimeout(75000);
 
-const delay = ({until: timeout}) => new Promise((resolve) => {
+const delay = ({ until: timeout }) => new Promise((resolve) => {
     setTimeout(() => {
         resolve();
     }, timeout);
 });
 
-if (properties && properties.deployedAppURL && properties.deployedAppURL !== '') {
+if (properties && properties.deployedAppURL && properties.deployedAppURL !== '' && properties.githubUsername && properties.githubUsername !== '') {
     maybe = test;
-    axios.defaults.baseURL = properties.deployedAppURL;
+
+    repo = properties.deployedAppURL;
+    owner = properties.githubUsername;
+    octokit = new Octokit({
+        auth: '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'
+    });
 }
 
 const getRandomTestData = async () => {
@@ -29,6 +38,68 @@ beforeAll(() => {
     return getRandomTestData();
 });
 
-maybe('GitHub Workflow - Scans for vulnerabilities', async () => {
+maybe('GitHub Workflow - Scan identifies safe images', async () => {
     
+    await octokit.request(`POST /repos/{${owner}}/{${repo}}/issues`, {
+        owner: owner,
+        repo: repo,
+        title: `Scan @ ${(new Date()).toUTCString()}`,
+        body: JSON.stringify(cases.safe)
+    });
+
+    // wait 45 seconds
+    await delay({until: 45000});
+
+    // scanned.json
+    const filePath = '../scanned.json';
+    const outputExists = await fileExists(filePath);
+    expect(outputExists).toBe(true);
+
+    const output = require(filePath);
+    const expected = cases.safe.map(c => ({'image': c, 'status': 'SAFE'}));
+    expect(output).toStrictEqual(expected);
 });
+
+maybe('GitHub Workflow - Scan identifies unsafe images', async () => {
+    
+    await octokit.request(`POST /repos/{${owner}}/{${repo}}/issues`, {
+        owner: owner,
+        repo: repo,
+        title: `Scan @ ${(new Date()).toUTCString()}`,
+        body: JSON.stringify(cases.unsafe)
+    });
+
+    // wait 45 seconds
+    await delay({until: 45000});
+
+    // scanned.json
+    const filePath = '../scanned.json';
+    const outputExists = await fileExists(filePath);
+    expect(outputExists).toBe(true);
+
+    const output = require(filePath);
+    const expected = cases.unsafe.map(c => ({'image': c, 'status': 'SAFE'}));
+    expect(output).toStrictEqual(expected);
+});
+
+// maybe('GitHub Workflow - Scan identifies safe and unsafe images', async () => {
+    
+//     await octokit.request(`POST /repos/{${owner}}/{${repo}}/issues`, {
+//         owner: owner,
+//         repo: repo,
+//         title: `Scan @ ${(new Date()).toUTCString()}`,
+//         body: [...cases.safe, ...cases.unsafe]
+//     });
+
+//     // wait 45 seconds
+//     await delay({until: 45000});
+
+//     // scanned.json
+//     const filePath = '../scanned.json';
+//     const outputExists = await fileExists(filePath);
+//     expect(outputExists).toBe(true);
+
+//     const output = require(filePath);
+//     const expected = cases.unsafe.map(c => ({'image': c, 'status': 'SAFE'}));
+//     expect(output).toStrictEqual(expected);
+// });
