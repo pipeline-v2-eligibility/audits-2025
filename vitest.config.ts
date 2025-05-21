f0f8f6a531515df64b8ec14da03668d336d98292
Charles Opute Odili <chaluwa@gmail.com>
2025-05-21 04:41:40 -0600

    update test scripts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vitest/config';
 
+const statsOutputDir = process.env.TEST_STATS_DIR || '';
+const statsFileName = `${statsOutputDir ? `${statsOutputDir}/` : ''}stats.json`;
+
 export default defineConfig({
     test: {
         environment: 'node',
         testTimeout: 20000,
-        setupFiles: './vitest.setup.ts',
         fileParallelism: false,
+        setupFiles: './vitest.setup.ts',
         reporters: [
-            'json'
-        ]
+            'json', 'default'
+        ],
+        outputFile: {
+            json: statsFileName,
+        }
     },
 });
